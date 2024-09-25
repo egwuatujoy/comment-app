9ac65a5654796aa160a61ad00d090d5fd074b3dd
joyegwuatu <egwuatujoy60@gmail.com>
2024-09-25 08:57:16 +0100

    created folder

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-yellow-400">
+        yhellooooooooooooooooooooooooooo
+      </h1>
+    </div>
+  );
+};
+
+export default App;
